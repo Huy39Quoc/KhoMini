a5d71a0711786219be23fb46da69ae19a1c1d4dc
ledangnguyen05 <ledangnguyena6@gmail.com>
2026-09-11 10:01:01 +0700

    Delete BE/KhoMini directory

--- BE/KhoMini/src/main/java/com/khomini/enums/PaymentType.java
+++
@@ -1,7 +0,0 @@
-package com.khomini.enums;
-
-public enum PaymentType {
-    DEPOSIT,
-    RENTAL_FEE,
-    EXTRA_CHARGE
-}
--- BE/KhoMini/src/main/java/com/khomini/enums/RoleName.java
+++
@@ -1,9 +0,0 @@
-package com.khomini.enums;
-
-public enum RoleName {
-    ROLE_CUSTOMER,
-    ROLE_STAFF,
-    ROLE_FACILITY_MANAGER,
-    ROLE_BUSINESS_MANAGER,
-    ROLE_ADMIN
-}
